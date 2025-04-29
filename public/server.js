bfb69fca263e789ab6a9061fe24adfa2a18802f2
mysmartdeal <sinyun82@gmail.com>
2025-04-29 21:39:33 +0900

    Update server.js

--- public/server.js
+++ public/server.js
@@ -1,13 +1,14 @@
+// public/server.js
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = 5000;
 
+// 11번가 레고 데이터 제공
 app.get('/get_products', (req, res) => {
-  const products = require('./11st_lego_products.json');  // JSON 파일 경로
-  res.json(products);
+    const products = require('./11st_lego_products.json');  // JSON 데이터 가져오기
+    res.json(products);  // 데이터를 JSON 형태로 응답
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
