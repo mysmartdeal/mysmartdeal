3dac39f32c9391c4c3178395d0fc0f36f7a61a1b
mysmartdeal <sinyun82@gmail.com>
2025-05-02 01:20:33 +0900

    Update MainSlider.js

--- components/MainSlider.js
+++ components/MainSlider.js
@@ -26,17 +26,12 @@ export default function MainSlider() {
       <Slider {...settings}>
         {slides.map((slide) => (
           <div key={slide.id} className="p-4">
-            <div className="bg-blue-100 rounded-lg shadow-md p-6 text-center">
-              <img
-                src={slide.img}
-                alt={slide.title}
-                className="h-[320px] w-full object-contain bg-white"
-                loading="lazy"
-              />
+            <div className="bg-blue-100 rounded-lg shadow-md p-6 text-center min-h-[300px]">
+              {/* <h2 className="text-xl font-bold mb-2">{slide.title}</h2> */}
+              <img src={slide.img} alt={slide.title} className="mx-auto max-h-60 object-contain" />
             </div>
           </div>
         ))}
       </Slider>
     </div>
   );
-}
