0dbd7c75af0d5d4846c2bf0eda5c0d8da6294e1a
mysmartdeal <sinyun82@gmail.com>
2025-05-02 02:43:41 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 
 import Layout from "../components/Layout";
-// import MainSlider from "../components/MainSlider";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -22,12 +21,11 @@ export default function Home() {
       {/* 광고 자리 placeholder (슬라이더 대신) */}
       <section className="container mx-auto mt-10 mb-10">
         <div className="h-[120px] bg-gray-200 rounded text-center flex items-center justify-center text-gray-500">
-          광고 자리 (Google AdSense 예정)
+          광고 자리 (//Google AdSense 예정)
         </div>
       </section>
 
-      {/* <MainSlider /> */}
-
+    
       <section className="container mx-auto mt-10">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {products.map((product, idx) => (
