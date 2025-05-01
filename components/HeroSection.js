64461dba4b6a5a48fa0d491e043472d85b99227a
mysmartdeal <sinyun82@gmail.com>
2025-05-02 02:03:38 +0900

    Update HeroSection.js

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function HeroSection() {
   return (
-    <section className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white py-24 text-center px-6">
+    <section className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white py-20 text-center px-6">
       <h1 className="text-4xl sm:text-5xl font-extrabold mb-4">
         레고 마니아를 위한 할인 큐레이션 플랫폼
       </h1>
