518c27e64a49e095c964219781c6a9c3921f2e04
mysmartdeal <sinyun82@gmail.com>
2025-05-03 01:10:08 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Google Analytics GA4 */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-322CF1104G`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-322CF1104G', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
