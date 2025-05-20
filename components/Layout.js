c24b27db5b1beee779d61c7be8d05488f1f12cd8
mysmartdeal <sinyun82@gmail.com>
2025-05-20 23:41:24 +0900

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -2,9 +2,21 @@ import HeroSection from "./HeroSection";
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <HeroSection />  {/* 상단 공통 Hero */}
-      {children}        {/* 페이지별 본문 */}
+    <div className="min-h-screen flex flex-col">
+      <HeroSection />
+      <main className="flex-grow">{children}</main>
+
+      <footer className="bg-white py-6 text-center text-gray-400 text-sm border-t">
+        &copy; 2025 MySmartDeal. All rights reserved.
+        <div className="mt-2 space-x-4">
+          <a href="/privacy-policy" className="text-blue-500 hover:underline">
+            개인정보처리방침
+          </a>
+          <a href="/contact" className="text-blue-500 hover:underline">
+            문의하기
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
