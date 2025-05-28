bd45a9da966098cf7f3a747e143fe7d3d0a9aeb0
mysmartdeal <sinyun82@gmail.com>
2025-05-28 21:29:46 +0900

    Update lotto.js

--- pages/lotto.js
+++ pages/lotto.js
@@ -240,7 +240,7 @@ export default function LottoPage() {
         <div className="mt-20 text-left max-w-5xl mx-auto">
           <h2 className="text-2xl font-bold mb-4 text-center">-최근 당첨 결과-</h2>
           <div className="text-xs text-gray-400 text-center mb-4">
-  ※ 당첨 결과 데이터 출처: 동행복권
+  ※ 당첨 결과 출처: 동행복권
 </div>
           <div className="flex flex-col items-center gap-6">
             {gallery.map((file, idx) => (
