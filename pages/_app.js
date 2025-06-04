1a6e49c0e827348c758684ad0d5cf36d1db616da
mysmartdeal <sinyun82@gmail.com>
2025-06-04 20:28:09 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,42 @@
 // pages/_app.js
 import "../styles/globals.css";
 import Script from "next/script";
-import Head from "next/head"; // ✅ Head 추가
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [excludeTracking, setExcludeTracking] = useState(false);
+
+  useEffect(() => {
+    if (document.cookie.includes("exclude_analytics=true")) {
+      setExcludeTracking(true);
+    }
+  }, []);
+
   return (
     <>
-      {/* ✅ 전체 페이지 파비콘 적용 */}
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      {/* ✅ 구글 애널리틱스 GA4 */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-322CF1104G`}
-      />
-      <Script
-  id="gtag-init"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      if (!document.cookie.includes("exclude_analytics=true")) {
-        gtag('config', 'G-322CF1104G', {
-          page_path: window.location.pathname,
-        });
-      }
-    `,
-  }}
-/>
-
-      {/* ✅ 실제 페이지 렌더링 */}
+      {!excludeTracking && typeof window !== "undefined" && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-322CF1104G`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-322CF1104G', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </>
+      )}
       <Component {...pageProps} />
     </>
   );
-}
+} 
