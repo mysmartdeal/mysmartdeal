05c3b950941ad0bd5d4543eac8e7a31d86434ace
mysmartdeal <sinyun82@gmail.com>
2025-06-05 00:03:47 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,46 @@
 // pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+function Document(props) {
+  const { excludeAnalytics } = props;
+
   return (
     <Html lang="ko">
       <Head>
-        {/* ✅ 여기 메타 태그 삽입 */}
-        <meta name="naver-site-verification" content="fdea2d57e62a836de83af6d940baedf51bf312e0" />
+        {!excludeAnalytics && (
+          <>
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-322CF1104G"
+            ></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-322CF1104G', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            ></script>
+          </>
+        )}
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
+
+Document.getInitialProps = async (ctx) => {
+  const initialProps = await ctx.defaultGetInitialProps(ctx);
+  const cookie = ctx.req?.headers?.cookie || "";
+  const excludeAnalytics = cookie.includes("exclude_analytics=true");
+  return { ...initialProps, excludeAnalytics };
+};
+
+export default Document;
